eb3e068657ca24d5ccb3c9132c5598161f01a847
dim <web.best.master@gmail.com>
2021-10-13 23:44:03 +0300

    2

---
+++ my-ui/Assets/Scenes/ButtonClick.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public void onClickButton()
+    {
+        Debug.Log("onClickButton = 1234");
+    }
+}
